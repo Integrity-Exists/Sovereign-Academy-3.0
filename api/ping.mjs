8e3d1073c664075a4560e6b73079d3ba4fbab687
Integrity-Exists.github.io <svc.with.integrity@gmail.com>
2025-08-26 21:02:02 -0400

    Create ping.mjs

---
+++ api/ping.mjs
@@ -0,0 +1,4 @@
+export default async function handler(req, res) {
+  if (req.method === 'GET') return res.status(200).json({ ok: true, runtime: process.version });
+  return res.status(405).json({ error: 'Method Not Allowed' });
+}
